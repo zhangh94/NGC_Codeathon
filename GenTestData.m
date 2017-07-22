b6fd61da777a31b73add5765ce803d96680e3e7d
Frank <zhangh94@gmail.com>
2017-07-22 00:53:47 -0700

    Initial commit

---
+++ GenTestData.m
@@ -0,0 +1,19 @@
+function [ struct ] = GenTestData( nCol, mRow, t, type )
+%FUNCTION to generate test data for a scenario of 'type' for t seconds
+%   Inputs
+%       nCol - number of columns for output data 
+%       nRow - number of rows for output data
+%       t - time in integer seconds to generate test data for
+%       type - scenario type to generate test data for
+
+% allocate array
+
+% loop through time steps
+
+% fill into struct
+
+
+
+
+end
+
